2faba5bec47665598325ee077b8def62856c7698
sonitsuka <onitsukashiho@gmail.com>
2022-06-14 17:03:42 +0200

    Add jsx basic file(empty contents)

---
+++ ecommerce/components/HeroBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroBanner() {
+  return (
+    <div className='hero-banner-container'>
+      <div>
+        </div>HeroBanner</div>
+  )
+}
+
+export default HeroBanner
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Product, FooterBanner, HeroBanner} from '../components'
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      <HeroBanner />
+      <div className='products-heading'>
+        <h2>Beset selling products</h2>
+        <p>text variations</p>
+      </div>
+
+      <div className='products-container'>
+        {['product 1', 'product 2'].map(
+          (product) => product
+        )}
+      </div>
+      
+      <FooterBanner />
+    </>
   )
 }
 
-export default index
+export default Home
